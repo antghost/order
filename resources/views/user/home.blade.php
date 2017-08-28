aa212fa4d7c50006b587f2de12964d404331dc54
tanuncle <tenfour15@qq.com>
2017-08-28 17:34:58 +0800

    make user dashboard

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-3">
             <ul class="nav nav-pills nav-stacked ">
                 <li role="presentation" class="active"><a href="{{ url('user') }}">概况</a></li>
-                <li role="presentation"><a href="{{ url('user/setting') }}">用餐设置</a></li>
+                <li role="presentation"><a href="{{ url('user/breakfast') }}">用餐设置</a></li>
                 <li role="presentation"><a href="{{ url('user/info') }}">用餐流水</a></li>
                 <li role="presentation"><a href="{{ url('user/comment') }}">管理</a></li>
             </ul>
@@ -23,24 +23,42 @@
                         </div>
                     @endif
 
-                    <div>
+                    <div class="col-md-12">
                         <p>
                             <strong>本人餐费标准</strong>
                             <label> 早餐：</label>10.00
                             <label> 午餐：</label>15.00
                             <label> 晚餐：</label>10.00
                         </p>
+                        <hr>
                     </div>
-                    <div>
-                        <h3>本月用餐天数</h3>
-                        <label> 早餐：</label>3
-                        <label> 午餐：</label>4
-                        <label> 晚餐：</label>8
-                        <h3>本月用餐费用</h3>
-                        <label> 早餐：</label>30.00
-                        <label> 午餐：</label>95.00
-                        <label> 晚餐：</label>50.00
-                        <p><label>费用合计：</label>385.00</p>
+
+                    <div class="col-md-12">
+                        <label>月份：</label>
+                        <select>
+                            <option>2017-08</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-12">
+                        <h4>天数</h4>
+                        <div class="col-md-4 text-center"><h2>3</h2></div>
+                        <div class="col-md-4 text-center"><h2>4</h2></div>
+                        <div class="col-md-4 text-center"><h2>8</h2></div>
+                        <div class="col-md-4 text-center"><label>早餐</label></div>
+                        <div class="col-md-4 text-center"><label>午餐</label></div>
+                        <div class="col-md-4 text-center"><label>晚餐</label></div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <h4>费用</h4>
+                        <div class="col-md-4 text-center"><h2>110.00</h2></div>
+                        <div class="col-md-4 text-center"><h2>285.00</h2></div>
+                        <div class="col-md-4 text-center"><h2>60.00</h2></div>
+                        <div class="col-md-4 text-center"><label>早餐</label></div>
+                        <div class="col-md-4 text-center"><label>午餐</label></div>
+                        <div class="col-md-4 text-center"><label>晚餐</label></div>
+                        <label>费用合计：</label>385.00 元
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 //用户功能
 Route::group(['namespace' => 'User', 'prefix' => 'user'], function (){
     Route::get('/', 'HomeController@index');
+    Route::get('/breakfast', 'BreakfastController@index');
 });
