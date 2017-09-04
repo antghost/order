3fd4bd5d5567d151896edf2c602f7656c43fa4f4
tanuncle <tenfour15@qq.com>
2017-09-04 17:38:38 +0800

    create demo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,9 +37,12 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;<li>
+                        &nbsp;
+                        @if(Auth::check())
+                        <li>
                             <a href="{{ url('/') }}">论坛</a>
                         </li>
+                        @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
