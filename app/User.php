beeaf6f12c8ba78a964086a0923f95022e86ac15
tanuncle <tenfour15@qq.com>
2017-09-13 23:23:33 +0800

    user

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Models\Dept', 'userdepts')->withTimestamps();
     }
+
+    public function dept()
+    {
+        return $this->belongsTo();
+    }
 }
