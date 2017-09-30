0cf1480e60457a13422965f050e262ed9ecebe6d
tanuncle <tenfour15@qq.com>
2017-09-30 16:53:00 +0800

    seeder

--- database/seeds/UserOrderStatusesSeeder.php
+++ database/seeds/UserOrderStatusesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 use App\User;
 
 class UserOrderStatusesSeeder extends Seeder
@@ -22,6 +23,43 @@ class UserOrderStatusesSeeder extends Seeder
         DB::table('cancel_dinners')->truncate();
         $users = User::all();
         foreach ($users as $user){
+            $beginDate = $user->priceUsers()->select('begin_date')->min('begin_date');
+            //用餐状态
+            $userOrder = $user->userOrderStatuses()->create([
+                'breakfast' => 1,
+                'lunch' => random_int(0, 1),
+                'dinner' => random_int(0, 1),
+            ]);
+            //早餐
+            if ($userOrder->breakfast){
+                $user->bookBreakfasts()->create([
+                    'begin_date' => $beginDate,
+                ]);
+            } else {
+                $user->cancelBreakfasts()->create([
+                    'begin_date' => $beginDate,
+                ]);
+            }
+            //午餐
+            if ($userOrder->lunch){
+                $user->bookLunches()->create([
+                    'begin_date' => $beginDate,
+                ]);
+            } else {
+                $user->cancelLunches()->create([
+                    'begin_date' => $beginDate,
+                ]);
+            }
+            //晚餐
+            if ($userOrder->dinner){
+                $user->bookDinners()->create([
+                    'begin_date' => $beginDate,
+                ]);
+            } else {
+                $user->CancelDinners()->create([
+                    'begin_date' => $beginDate,
+                ]);
+            }
         }
     }
 }
