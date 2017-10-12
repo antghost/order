a7ab4bcd05d11031f617debbece59bfd785cf5ec
tanuncle <tenfour15@qq.com>
2017-10-12 17:45:37 +0800

    增加日期控件

--- app/Http/Controllers/Staff/LimitController.php
+++ app/Http/Controllers/Staff/LimitController.php
@@ -4,11 +4,50 @@ namespace App\Http\Controllers\staff;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\OrderTime;
 
 class LimitController extends Controller
 {
     public function index()
     {
-        return view('staff.limit.index');
+        //早餐时限
+        $breakfastTime = OrderTime::where('type', 1)->first();
+        //午餐时限
+        $lunchTime = OrderTime::where('type', 2)->first();
+        //晚餐时限
+        $dinnerTime = OrderTime::where('type', 3)->first();
+        return view('staff.limit.index',[
+            'breakfastTime' => $breakfastTime,
+            'lunchTime' => $lunchTime,
+            'dinnerTime' => $dinnerTime,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        //类型
+        $type = $request->input('type');
+        //开餐时间
+        $bookTime = $request->input('book_time');
+        //停餐时间
+        $cancelTime = $request->input('cancel_time');
+
+        $this->validate($request, [
+            'type' => 'required|min:1|max:3|numeric',
+            'book_time' => 'required',
+            'cancel_time' => 'required',
+        ]);
+
+        $orderTime = OrderTime::updateOrCreate(
+            ['type' => $type],
+            ['book_time' => $bookTime, 'cancel_time' => $cancelTime]
+        );
+
+        if (isset($orderTime)){
+            return redirect()->back()->with('status', '修改完成');
+        } else {
+            return redirect()->back()->withInput()->withErrors('提交失败');
+        }
+
     }
 }
--- resources/views/staff/limit/index.blade.php
+++ resources/views/staff/limit/index.blade.php
@@ -9,82 +9,125 @@
 
             <div class="col-md-9 col-md-offset-0">
                 <div class="panel panel-info">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <div class="panel panel-heading">早餐</div>
-
                     <div class="panel-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
                         <div class="col-md-12">
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <th>定餐时限</th>
-                                    <th>停餐时限</th>
-                                    <th>操作</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>2017-01-01 10:00</td>
-                                    <td>2017-11-05 15:00</td>
-                                    <td>
-                                        <button class="btn btn-info">编辑</button>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
+                            <form name="breakfast_form" action="{{ url('staff/limit') }}" method="post">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="type" value="1">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th>开餐时限</th>
+                                        <th>停餐时限</th>
+                                        <th>操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <input type="text" name="book_time" required
+                                                   value="{{ $breakfastTime->book_time or '' }}"
+                                                   onclick="WdatePicker({dateFmt:'H:mm:ss', minTime:'08:30:00', maxTime:'17:30:00'})">
+                                        </td>
+                                        <td>
+                                            <input type="text" name="cancel_time" required
+                                                   value="{{ $breakfastTime->cancel_time or '' }}"
+                                                   onclick="WdatePicker({dateFmt:'H:mm:ss', minTime:'08:30:00', maxTime:'17:30:00'})">
+                                        </td>
+                                        <td>
+                                            <button type="submit" class="btn btn-info">保存</button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </form>
                         </div>
                     </div>
 
                     <div class="panel-heading">午餐</div>
                     <div class="panel-body">
                         <div class="col-md-12">
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <th>定餐时限</th>
-                                    <th>停餐时限</th>
-                                    <th>操作</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>2017-01-01 10:00</td>
-                                    <td>2017-11-05 15:00</td>
-                                    <td>
-                                        <button class="btn btn-info">编辑</button>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
+                            <form name="lunch_form" action="{{ url('staff/limit') }}" method="post">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="type" value="2">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th>开餐时限</th>
+                                        <th>停餐时限</th>
+                                        <th>操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <input type="text" name="book_time" required
+                                                   value="{{ $lunchTime->book_time or '' }}"
+                                                   onclick="WdatePicker({dateFmt:'H:mm:ss', minTime:'08:30:00', maxTime:'17:30:00'})">
+                                        </td>
+                                        <td>
+                                            <input type="text" name="cancel_time" required
+                                                   value="{{ $lunchTime->cancel_time or '' }}"
+                                                   onclick="WdatePicker({dateFmt:'H:mm:ss', minTime:'08:30:00', maxTime:'17:30:00'})">
+                                        </td>
+                                        <td>
+                                            <button type="submit" class="btn btn-info">保存</button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </form>
                         </div>
                     </div>
 
                     <div class="panel-heading">晚餐</div>
                     <div class="panel-body">
                         <div class="col-md-12">
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <th>定餐时限</th>
-                                    <th>停餐时限</th>
-                                    <th>操作</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>2017-01-01 10:00</td>
-                                    <td>2017-11-05 15:00</td>
-                                    <td>
-                                        <button class="btn btn-info">编辑</button>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
+                            <form name="dinner_form" action="{{ url('staff/limit') }}" method="post">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="type" value="3">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th>开餐时限</th>
+                                        <th>停餐时限</th>
+                                        <th>操作</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <input type="text" name="book_time" required
+                                                   value="{{ $dinnerTime->book_time or '' }}"
+                                                   onclick="WdatePicker({dateFmt:'H:mm:ss', minTime:'08:30:00', maxTime:'17:30:00'})">
+                                        </td>
+                                        <td>
+                                            <input type="text" name="cancel_time" required
+                                                   value="{{ $dinnerTime->cancel_time or '' }}"
+                                                   onclick="WdatePicker({dateFmt:'H:mm:ss', minTime:'08:30:00', maxTime:'17:30:00'})">
+                                        </td>
+                                        <td>
+                                            <button type="submit" class="btn btn-info">保存</button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </form>
                         </div>
                     </div>
 
@@ -93,4 +136,8 @@
 
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script src="{{asset('My97DatePicker/WdatePicker.js')}}"></script>
 @endsection
