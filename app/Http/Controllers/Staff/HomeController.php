74498bb9ae3db2fbd0852319ea05b00970604233
tanuncle <tenfour15@qq.com>
2017-11-17 17:29:52 +0800

    图表

--- app/Http/Controllers/Staff/HomeController.php
+++ app/Http/Controllers/Staff/HomeController.php
@@ -131,29 +131,32 @@ class HomeController extends Controller
 
         $userCount = 0;
         //情况1：$startDate：10月1日，$endDate：10月31日，数据记录开始与结束日期都为10月
-        $oneCount = $meal::where([
+        $oneCount = $meal::select('user_id')->where([
             ['begin_date', '>=', $startDate],
             ['end_date', '<=', $endDate],
-        ])->distinct('user_id')->count('user_id');
+        ]);
+//            ->distinct('user_id')->count('user_id');
 
         //情况2：$startDate：10月1日，$endDate：10月31日，数据记录开始日期为9月或之前，结束日期为10月或之后(理论上应该只有1条这样的数据)
-        $twoCount = $meal::where('begin_date', '<', $startDate)
+        $twoCount = $meal::select('user_id')
+            ->where('begin_date', '<', $startDate)
             ->where(function ($query) use ($startDate){
                 $query->where('end_date', '>=', $startDate)
                     ->orWhereNull('end_date');
-            })
-            ->distinct('user_id')->count('user_id');
+            });
+//            ->distinct('user_id')->count('user_id');
 
         //情况3：$startDate：10月1日，$endDate：10月31日，数据记录开始日期为10月，结束日期为11月或之后(理论上应该只有1条这样的数据)
-        $threeCount = $meal::where([
+        $threeCount = $meal::select('user_id')->where([
             ['begin_date', '<=', $endDate],
             ['begin_date', '>=', $startDate]])
             ->where(function ($query) use($endDate){
                 $query->where('end_date', '>', $endDate)
                     ->orWhereNull('end_date');
-            })
-            ->distinct('user_id')->count('user_id');
+            })->union($twoCount)->union($oneCount)->get();
+//            ->distinct('user_id')->count('user_id');
+        $userCount = $threeCount->count();
 
-        return $userCount = $oneCount + $twoCount + $threeCount;
+        return $userCount;
     }
 }
--- resources/views/staff/report/index.blade.php
+++ resources/views/staff/report/index.blade.php
@@ -33,7 +33,7 @@
                         </form>
 
                         <div class="col-md-12">
-                            <div id="echarts" style="width: auto; height: 450px"></div>
+                            <div id="echarts" style="width: auto; height: 900px"></div>
                         </div>
 
                     </div>
@@ -57,19 +57,55 @@
                 option = {
                     tooltip: {
                         trigger: 'axis',
+                        formatter: year+'年{b}<br />{a0}: {c0}<br />{a1}: {c1}<br />{a2}: {c2}<br />',
                         axisPointer: {            // 坐标轴指示器，坐标轴触发有效
                             type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
                         }
                     },
                     legend: {
+                        type: 'scroll',
+                        right: '13%',
+//                        orient: 'vertical',
                         data: ['早餐人数', '早餐餐数', '午餐人数', '午餐餐数', '晚餐人数', '晚餐餐数', '早餐总金额', '午餐总金额', '晚餐总金额']
                     },
-                    grid: {
+//                    dataZoom: [
+//                        {
+//                            show: true,
+//                            realtime: true,
+//                            start: 0,
+//                            end: 100,
+//                            xAxisIndex: [0, 1, 2]
+//                        },
+//                        {
+//                            type: 'inside',
+//                            realtime: true,
+//                            start: 0,
+//                            end: 100,
+//                            xAxisIndex: [0, 1, 2]
+//                        }
+//                    ],
+                    grid: [{
+                        left: '5%',
+                        right: '5%',
+                        bottom: '3%',
+                        top: '10%',
+                        height: '25%',
+                        containLabel: true
+                    }, {
+                        left: '4%',
+                        right: '5%',
+                        bottom: '3%',
+                        top: '40%',
+                        height: '25%',
+                        containLabel: true
+                    }, {
                         left: '3%',
                         right: '5%',
                         bottom: '3%',
+                        top: '70%',
+                        height: '25%',
                         containLabel: true
-                    },
+                    }],
                     toolbox: {
                         feature: {
                             dataView: {show: true, readOnly: true},
@@ -77,30 +113,95 @@
                             saveAsImage: {show: true}
                         }
                     },
-                    xAxis: {
+                    xAxis: [{
                         type: 'category',
+                        gridIndex: 0,
                         axisTick: {
                             alignWithLabel: true
                         },
                         data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
-                    },
+                    }, {
+                        type: 'category',
+                        gridIndex: 1,
+                        axisTick: {
+                            alignWithLabel: true
+                        },
+                        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+                    }, {
+                        type: 'category',
+                        gridIndex: 2,
+                        axisTick: {
+                            alignWithLabel: true
+                        },
+                        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+                    }],
                     yAxis: [{
-                        type: 'value'
-                    },
-                        {
-                            type: 'value',
-                            name: '金额',
-                            min: 0,
-                            max: 60000,
-                            interval: 10000,
-                            axisLabel: {
-                                formatter: '{value}元'
-                            }
+                        gridIndex: 0,
+                        type: 'value',
+                        name: '用餐人数',
+                    },{
+                        gridIndex: 1,
+                        type: 'value',
+                        name: '用餐餐数',
+                    }, {
+                        gridIndex: 2,
+                        type: 'value',
+                        name: '金额',
+//                        min: 0,
+//                        max: 60000,
+//                        interval: 10000,
+                        axisLabel: {
+                            formatter: '{value}元'
                         }
+                    }
                     ],
                     series: [
                         {
+                            name: '早餐人数',
+                            xAxisIndex: 0,
+                            yAxisIndex: 0,
+                            type: 'bar',
+                            stack: '总人数',    //数据堆叠，同个类目轴上系列配置相同的stack值可以堆叠放置。
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'insideRight'
+                                }
+                            },
+                            data: data.breakfast_users
+                        },
+                        {
+                            name: '午餐人数',
+                            xAxisIndex: 0,
+                            yAxisIndex: 0,
+                            type: 'bar',
+                            stack: '总人数',
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'insideRight'
+                                }
+                            },
+                            data: data.lunch_users
+                        },
+                        {
+                            name: '晚餐人数',
+                            xAxisIndex: 0,
+                            yAxisIndex: 0,
+                            type: 'bar',
+                            stack: '总人数',
+                            label: {
+                                normal: {
+                                    show: true,
+                                    position: 'insideRight'
+                                }
+                            },
+                            data: data.dinner_users
+                        },
+                        {
                             name: '早餐餐数',
+                            xAxisIndex: 1,
+                            yAxisIndex: 1,
                             type: 'bar',
                             stack: '总餐数',    //数据堆叠，同个类目轴上系列配置相同的stack值可以堆叠放置。
                             label: {
@@ -113,6 +214,8 @@
                         },
                         {
                             name: '午餐餐数',
+                            xAxisIndex: 1,
+                            yAxisIndex: 1,
                             type: 'bar',
                             stack: '总餐数',
                             label: {
@@ -125,6 +228,8 @@
                         },
                         {
                             name: '晚餐餐数',
+                            xAxisIndex: 1,
+                            yAxisIndex: 1,
                             type: 'bar',
                             stack: '总餐数',
                             label: {
@@ -135,58 +240,25 @@
                             },
                             data: data.dinners
                         },
-//                {
-//                    name: '早餐人数',
-//                    type: 'bar',
-//                    stack: '总人数',    //数据堆叠，同个类目轴上系列配置相同的stack值可以堆叠放置。
-//                    label: {
-//                        normal: {
-//                            show: true,
-//                            position: 'insideRight'
-//                        }
-//                    },
-//                    data: data.breakfast_users
-//                },
-//                {
-//                    name: '午餐人数',
-//                    type: 'bar',
-//                    stack: '总人数',
-//                    label: {
-//                        normal: {
-//                            show: true,
-//                            position: 'insideRight'
-//                        }
-//                    },
-//                    data: data.lunche_users
-//                },
-//                {
-//                    name: '晚餐人数',
-//                    type: 'bar',
-//                    stack: '总人数',
-//                    label: {
-//                        normal: {
-//                            show: true,
-//                            position: 'insideRight'
-//                        }
-//                    },
-//                    data: data.dinner_users
-//                },
                         {
                             name: '早餐总金额',
                             type: 'line',
-                            yAxisIndex: 1,
+                            xAxisIndex: 2,
+                            yAxisIndex: 2,
                             data: data.breakfast_amount
                         },
                         {
                             name: '午餐总金额',
                             type: 'line',
-                            yAxisIndex: 1,
+                            xAxisIndex: 2,
+                            yAxisIndex: 2,
                             data: data.lunch_amount
                         },
                         {
                             name: '晚餐总金额',
                             type: 'line',
-                            yAxisIndex: 1,
+                            xAxisIndex: 2,
+                            yAxisIndex: 2,
                             data: data.dinner_amount
                         }
                     ]
