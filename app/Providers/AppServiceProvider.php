f59a14463f0b22e70f96a813b36ad48aef958a94
tanuncle <tenfour15@qq.com>
2017-11-19 22:33:30 +0800

    SQL输出

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //监控程序执行的每一条 SQL 语句
+        DB::listen(function ($query){
+            Log::info($query->sql);
+            Log::info($query->bindings);
+        });
     }
 
     /**
